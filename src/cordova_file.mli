aacfb80766d7ed9d2ac72022998156d91a42a9ae
Thibaut <tgudin@besport.com>
2021-09-02 14:34:42 +0200

    adding 'get_error_code_exn' function + README fixups

--- src/cordova_file.mli
+++ src/cordova_file.mli
@@ -58,6 +58,31 @@ type file = Js_of_ocaml.File.file Js_of_ocaml.Js.t
 
 val get_error_code : error -> int [@@js.get "code"]
 
+[@@@js.stop]
+
+val get_error_code_exn : error -> exn
+
+[@@@js.start]
+
+[@@@js.implem
+let get_error_code_exn e =
+  let code = get_error_code e in
+  match code with
+  | 1 -> Not_found
+  | 2 -> Failure "Security Error"
+  | 3 -> Failure "Abort Error"
+  | 4 -> Invalid_argument "Not Readable Error"
+  | 5 -> Invalid_argument "Encoding Error"
+  | 6 -> Failure "No Modification Allowed Error"
+  | 7 -> Failure "Invalid State Error"
+  | 8 -> Invalid_argument "Syntaxe Error"
+  | 9 -> Failure "Invalid Modification Error"
+  | 10 -> Failure "Quota Exceeded Error"
+  | 11 -> Failure "Typing Mismatch Error"
+  | 12 -> Failure "Path Exists Error"
+  (*Their is only 12 ""ode Error"" known*)
+  | _ -> Match_failure ("cordova_file.mli", 70, 2)]
+
 module File_entry : sig
   type t
 
